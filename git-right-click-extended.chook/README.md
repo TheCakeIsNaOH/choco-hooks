5674f6abb9cf5e83794d0b4babf82eb89456be50
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-03-26 20:56:25 -0500

    add git right click extended

---
+++ git-right-click-extended.chook/README.md
@@ -0,0 +1,3 @@
+Makes the Git right click menu extended, thus making it only show up on shift+click
+
+It resets every time git upgrades, so this runs every time git installs
---
+++ git-right-click-extended.chook/hook/post-install-git.install.ps1
@@ -0,0 +1,16 @@
+﻿if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\Background\shell\git_shell") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\Background\shell\git_shell" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\Background\shell\git_gui") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\Background\shell\git_gui" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\shell\git_gui") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\shell\git_gui" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\shell\git_shell") -ne $true) {
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\shell\git_shell" -force -ea SilentlyContinue 
+};
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\Background\shell\git_shell' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\Background\shell\git_gui' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\shell\git_gui' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\shell\git_shell' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
