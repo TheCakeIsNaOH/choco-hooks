0cb65420757b8b5b132fdacdcc65e73e1c67a77c
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-06-14 20:28:05 -0500

    Add vscodium-right-click-extended.chook

---
+++ vscodium-right-click-extended.chook/README.md
@@ -0,0 +1,3 @@
+Makes the VSCodium right click menu extended, thus making it only show up on shift+click
+
+It resets every time VSCodium upgrades, so this runs every time VSCodium installs
---
+++ vscodium-right-click-extended.chook/hook/post_install_vscodium.install.ps1
@@ -0,0 +1,12 @@
+﻿if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\*\shell\VSCodium") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCodium" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCodium") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCodium" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\shell\VSCodium") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\shell\VSCodium" -force -ea SilentlyContinue 
+};
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCodium' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\shell\VSCodium' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\*\shell\VSCodium' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
