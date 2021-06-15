6e4d24ab4f02d2e45eda173ea97933a993a678e3
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-06-14 20:25:28 -0500

    Add vscode-right-click-extended.chook

---
+++ vscode-right-click-extended.chook/README.md
@@ -0,0 +1,3 @@
+Makes the VS Code right click menu extended, thus making it only show up on shift+click
+
+It resets every time VS Code upgrades, so this runs every time VS Code installs
---
+++ vscode-right-click-extended.chook/hook/post_install_vscode.install.ps1
@@ -0,0 +1,12 @@
+﻿if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\*\shell\VSCode") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCode" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCode") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCode" -force -ea SilentlyContinue 
+};
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\Directory\shell\VSCode") -ne $true) {  
+    New-Item "HKLM:\SOFTWARE\Classes\Directory\shell\VSCode" -force -ea SilentlyContinue 
+};
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\Background\shell\VSCode' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\Directory\shell\VSCode' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
+$null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\*\shell\VSCode' -Name 'Extended' -Value '' -PropertyType String -Force -ea SilentlyContinue;
